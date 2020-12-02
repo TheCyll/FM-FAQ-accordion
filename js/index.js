4dbd68f15c26ba20838b6ba602153913b21153db
Hazel <hazelwence_13@hotmail.com>
2020-12-02 03:43:00 -0600

    Structure clarity and usage of node replacement

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 const details = document.querySelectorAll('details');
 const summary = document.querySelectorAll('summary');
+const principalContainer = document.querySelector('.principal-container');
+const principal = document.querySelector('.principal');
+const shadow = document.querySelector('#shadow');
+const images = document.querySelector('.images');
 const arroba = document.getElementById('arroba-img');
 const media = window.matchMedia("(max-width: 420px)");
- 
 
+// Change the order of the nodes, for better design implementation
+if(media.matches){
+    let newShadow = shadow.cloneNode(true);
+    shadow.remove();
+    principal.prepend(newShadow);
+    let newImages = images.cloneNode(true);
+    images.remove();
+    principalContainer.prepend(newImages);
+}
+ 
+// For mouse hovering, with media querys covered 
 summary.forEach( (sum) => {
     sum.addEventListener('mouseover', () => { 
         if(media.matches){
-            arroba.style.left = "-171px";  
+            arroba.style.left = "-10px";  
             arroba.style.transition = "all 0.5s" 
         }else{
             arroba.style.left = "-572px";  
@@ -16,13 +30,14 @@ summary.forEach( (sum) => {
     });
     sum.addEventListener('mouseout', () => {   
         if(media.matches){
-            arroba.style.left = "-161px";
+            arroba.style.left = "0";
         }else{
             arroba.style.left = "-552px";
         }     
     });
 });
 
+// For closing the other details when one is clicked
 details.forEach( targetDetail => {
     targetDetail.addEventListener('click', () => {
         details.forEach( detail => {
